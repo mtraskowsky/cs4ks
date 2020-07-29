69f40a775be9fc210bb40f77e5945da07d5a5550
Maria Traskowsky <mariatraskowsky@ksu.edu>
2020-07-29 20:06:08 +0000

    finished project 5

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const handleRequest = require('./src/handle-request');
 const http = require('http');
 require('./src/database');
 require('./src/templates');
 
+const app = require('./src/app');
+
 const port = 3000;
 
+// Start listening for requests
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+/* from PROJ4: replacing this with the express implementation
 var server = http.createServer(handleRequest);
 
 server.listen(port, function(){
   console.log("Server is listening on port " + port);
 });
+*/
+
--- src/serve-standards.js
+++ src/serve-standards.js
@@ -11,24 +11,24 @@ const Database = require('better-sqlite3');
 function serveStandards(req, res) {
   //TODO serve standards
   var getAllStandards = db.prepare(`SELECT 
-  standards.identifier AS identifier,
-  standards.description AS description,
-  standards.summary AS summary,
-  concepts.name AS concept,
-  concepts.abbr AS conceptAbbr,
-  subconcepts.name AS subconcept,
-  subconcepts.abbr AS subconceptAbbr,
-  grade_levels.name AS gradeLevel,
-  grade_levels.abbr AS gradeLevelAbbr,
-  (  SELECT group_concat(practices.id || '. ' || practices.name, ', ') 
-     FROM practices 
-     INNER JOIN standards_practices ON practices.id = standards_practices.practice_id 
-   WHERE standards_practices.standard_id = standards.id
-  ) AS practices
-FROM standards 
-INNER JOIN concepts on standards.concept_id = concepts.id
-INNER JOIN subconcepts on standards.subconcept_id = subconcepts.id 
-INNER JOIN grade_levels on standards.grade_level_id = grade_levels.id`).all();
+    standards.identifier AS identifier,
+    standards.description AS description,
+    standards.summary AS summary,
+    concepts.name AS concept,
+    concepts.abbr AS conceptAbbr,
+    subconcepts.name AS subconcept,
+    subconcepts.abbr AS subconceptAbbr,
+    grade_levels.name AS gradeLevel,
+    grade_levels.abbr AS gradeLevelAbbr,
+    (  SELECT group_concat(practices.id || '. ' || practices.name, ', ') 
+       FROM practices 
+       INNER JOIN standards_practices ON practices.id = standards_practices.practice_id 
+     WHERE standards_practices.standard_id = standards.id
+    ) AS practices
+  FROM standards 
+  INNER JOIN concepts on standards.concept_id = concepts.id
+  INNER JOIN subconcepts on standards.subconcept_id = subconcepts.id 
+  INNER JOIN grade_levels on standards.grade_level_id = grade_levels.id`).all();
 
   var standards = {standards: getAllStandards};
   
